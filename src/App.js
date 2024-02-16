eb247f1a3386ca3fd7c319db061e803916da7c30
YoungseoChoi23 <dudtj9326@ewha.ac.kr>
2024-02-17 04:33:37 +0900

    feat : 일기 개수 표시 기능 추가, useMemo를 이용한 연산 최적화

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
@@ -6,6 +6,27 @@ import DiaryList from './DiaryList';
 function App() {
   const [data, setData] = useState([]);
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch(
+      'https://jsonplaceholder.typicode.com/comments'
+    ).then((res) => res.json()); //res에는 객체 배열이 저장되어 있다.
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        create_dated: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+    setData(initData);
+  }; //API 호출 내장함수 fetch 사용
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const create_dated = new Date().getTime();
@@ -36,9 +57,26 @@ function App() {
     );
   };
 
+  const getDiaryAnalysis = useMemo(() => {
+    console.log('일기 분석 시작');
+
+    const goodCount = data.filter((it) => it.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+
+    return { goodCount, badCount, goodRatio }; //getDiaryAnalysis의 반환값은 객체
+  }, [data.length]);
+
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+  //getDiaryAnalysis는 UseMemo의 반환값인 객체를 가지고 있는 상수
+
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
+      <div>전체 일기 : {data.length}</div>
+      <div>기분 좋은 일기 개수 : {goodCount}</div>
+      <div>기분 나쁜 일기 개수 : {badCount}</div>
+      <div>기분 좋은 일기 비율 : {goodRatio} %</div>
       <DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit} />
     </div>
   );
